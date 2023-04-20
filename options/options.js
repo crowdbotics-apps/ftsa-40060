988a6267c6c98a6d3327845c490061fe095d78fc
Crowdbotics <team@crowdbotics.com>
2023-04-20 20:05:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ftsa-40060",
+  url: "https://ftsa-40060.botics.co",
+  api: "https://ftsa-40060.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
